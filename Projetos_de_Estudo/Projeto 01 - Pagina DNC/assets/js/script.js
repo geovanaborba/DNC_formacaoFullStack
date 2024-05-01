7dc111e35a7d05f97103345f7098172f92ece4da
Geovana Borba <geborba19@hotmail.com>
2024-05-01 19:39:15 -0300

    fix: corrige posição dos cards e setas na seção de depoimentos

--- Projetos_de_Estudo/Projeto 01 - Pagina DNC/assets/js/script.js
+++ Projetos_de_Estudo/Projeto 01 - Pagina DNC/assets/js/script.js
@@ -1,21 +1,24 @@
 var cardLeo = document.getElementById('card-leo');
 var cardBruna = document.getElementById('card-bruna');
 var cardSam = document.getElementById('card-sam');
-// var left = document.getElementById('left');
-// var right = document.getElementById('right');
+var left = document.getElementById('left');
+var right = document.getElementById('right');
+var carousel = document.getElementById('cards');
 
 function rollRight() {
     cardBruna.style = 'display:none';
     cardLeo.style = 'display:flex';
     cardSam.style = 'display:flex';
-    // right.style = 'display:none';
-    // left.style = 'display:flex';
+    right.style = 'display:none';
+    left.style = 'display:flex';
+    carousel.style = 'margin-right:100px; margin-left:0';
 }
 
 function rollLeft() {
     cardBruna.style = 'display:flex';
     cardLeo.style = 'display:flex';
     cardSam.style = 'display:none';
-    // right.style = 'display:flex';
-    // left.style = 'display:none';
+    right.style = 'display:flex';
+    left.style = 'display:none';
+    carousel.style = 'margin-right:0; margin-left:100px';
 }
