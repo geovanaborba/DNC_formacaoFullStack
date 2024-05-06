4d635a3f3f6c6de748fefd850316f63de4b74e79
Geovana Borba <geborba19@hotmail.com>
2024-05-06 18:57:00 -0300

    feat: adiciona formulário - Desafio 01

---
+++ Desafios/Desafio 01 - Landing Page/assets/js/script.js
@@ -0,0 +1,12 @@
+var slide = {
+    origin: 'left',
+    distance: '100px',
+    scale: 0.80,
+    duration: 2000,
+    reset: true,
+    delay: 300,
+};
+
+ScrollReveal().reveal('header h1', slide);
+ScrollReveal().reveal('header h5', slide);
+
